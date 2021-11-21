59f9004274e809e6965a7a527326f05d7e95ba6d
v1shalm <mauryav518@gmail.com>
2021-11-21 16:30:14 +0530

    first commit

---
+++ components/Author.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image';
+
+const Author = ({author}) => {
+    return (
+        <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-yellow-600 bg-opacity-20">
+            <div className="absolute left-0 right-0 -top-14">
+                <Image
+                    alt={author.name}
+                    unoptimized
+                    height="100px"
+                    width="100px"
+                    className="align-middle rounded-full"
+                    src={author.photo.url}
+                 />
+                 </div>
+            <h3 className="text-black my-4 text-2xl font-bold">
+                {author.name}
+            </h3>
+            <p className="text-black text-lg font-bold">{author.bio}</p>
+        
+
+ </div>
+    )
+}
+
+export default Author
